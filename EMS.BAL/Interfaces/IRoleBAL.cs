ed290c2eedd3d6d20f369ca2211a1609e9098a74
anish-tezo-104 <anish.m@Technovert.com>
2024-05-15 12:16:54 +0530

    implemented web api routings

--- EMS.BAL/Interfaces/IRoleBAL.cs
+++
@@ -1,9 +0,0 @@
-using EMS.DB.Models;
-
-namespace EMS.BAL.Interfaces;
-
-public interface IRoleBAL
-{
-    public int AddRole(Role role);
-    public List<Role>? GetAll();
-}
--- EMS.DAL/Interfaces/IRoleDAL.cs
+++
@@ -1,9 +0,0 @@
-using EMS.DB.Models;
-
-namespace EMS.DAL.Interfaces;
-
-public interface IRoleDAL
-{
-    public int Insert(Role role);
-    public List<Role>? RetrieveAll();
-}
